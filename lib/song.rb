216d86f53d0927e1b99c253ba6befd10cf5557de
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-08 16:35:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class Song 
+  attr_reader :name, :artist, :genre
+  @@count= 0
+  @@artists =[]
+  @@genres=[]
+  def initialize (name, artist, genre)
+         @name = name
+         @artist= artist
+         @genre = genre
+         @@count += 1 
+         @@artists << artist
+         @@genres << genre
+  end
+ 
+ def self.count
+   @@count
+ end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count 
+    hash={}
+    @@genres.each do |k|
+      if hash[k]
+        hash[k] += 1
+      else
+        hash[k]= 1
+      end
+   
+    end
+         hash
+  end
+end
